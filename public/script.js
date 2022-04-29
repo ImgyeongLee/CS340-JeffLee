5d7cd9875ba745f9fb45ab28a98a6ce7e0b7d361
Imgyeong Lee <leeim@oregonstate.edu>
2022-04-28 23:56:13 -0700

    Add static functions

---
+++ public/script.js
@@ -0,0 +1,61 @@
+function goHome() {
+  var link = "./home.html";
+  window.location.href = link;
+}
+
+function goDiagnosis() {
+  var link = "./diagnosis.html";
+  window.location.href = link;
+}
+
+function goDoctor() {
+  var link = "./doctor.html";
+  window.location.href = link;
+}
+
+function goMedi() {
+  var link = "./medication.html";
+  window.location.href = link;
+}
+
+function goPatient() {
+  var link = "./patient.html";
+  window.location.href = link;
+}
+
+function goPharm() {
+  var link = "./pharmacy.html";
+  window.location.href = link;
+}
+
+function openInsert() {
+  document.getElementById('insert-box').style.display="block"
+}
+
+function closeInsert() {
+  document.getElementById('insert-box').style.display="none"
+}
+
+function openDelete() {
+  document.getElementById('delete-box').style.display="block"
+}
+
+function closeDelete() {
+  document.getElementById('delete-box').style.display="none"
+}
+
+function openSearch() {
+  document.getElementById('search-box').style.display="block"
+}
+
+function closeSearch() {
+  document.getElementById('search-box').style.display="none"
+}
+
+function openUpdate() {
+  document.getElementById('update-box').style.display="block"
+}
+
+function closeUpdate() {
+  document.getElementById('update-box').style.display="none"
+}
