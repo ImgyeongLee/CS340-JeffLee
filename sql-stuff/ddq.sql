db6341a408cdb783952b931d9b96fdabbd70be4b
solderq35 <solderq35@gmail.com>
2022-05-13 12:09:38 -0700

    fix foreign key checks again, improve example inserted values

--- sql-stuff/ddq.sql
+++ sql-stuff/ddq.sql
@@ -1,4 +1,4 @@
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 
 DROP TABLE IF EXISTS `patient`;
 CREATE TABLE patient (
@@ -13,7 +13,7 @@ CREATE TABLE patient (
 	
 LOCK TABLES `patient` WRITE;
 /*!40000 ALTER TABLE `patient` DISABLE KEYS */;
-INSERT INTO `patient`(patient_first_name,patient_last_name,patient_birth,patient_address,patient_email,patient_contact) VALUES ('John','Cena','1969-4-20','7292 Dictum Av.San Antonio MI 47096','jcena@gmail.com','503-888-4432'),('Larry','David','1981-6-23','3748 Jorge St.Austin TX 28732','ldavid@gmail.com','222-345-6758');
+INSERT INTO `patient`(patient_first_name,patient_last_name,patient_birth,patient_address,patient_email,patient_contact) VALUES ('John','Cena','1969-4-20','7292 SW Dictum Av. San Antonio MI 47096','jcena@gmail.com','503-888-4432'),('Larry','David','1981-6-23','3748 NW Jorge St. Austin TX 28732','ldavid@gmail.com','222-345-6758');
 /*!40000 ALTER TABLE `patient` ENABLE KEYS */;
 UNLOCK TABLES;	
 
@@ -42,7 +42,7 @@ CREATE TABLE pharmacy (
 	) ENGINE=InnoDB;
 LOCK TABLES `pharmacy` WRITE;
 /*!40000 ALTER TABLE `pharmacy` DISABLE KEYS */;
-INSERT INTO `pharmacy`(pharmacy_name,pharmacy_address,pharmacy_contact) VALUES ('Wallgreens','Portland, OR','334-233-4839'),('Fred Meyer Pharmacy','Corvallis','234-382-3938');
+INSERT INTO `pharmacy`(pharmacy_name,pharmacy_address,pharmacy_contact) VALUES ('Wallgreens','2293 SE Jefferson St. Portland, OR','334-233-4839'),('Fred Meyer Pharmacy','3354 SW Washington Ave. Corvallis','234-382-3938');
 /*!40000 ALTER TABLE `pharmacy` ENABLE KEYS */;
 UNLOCK TABLES;		
 
@@ -52,13 +52,13 @@ CREATE TABLE `medication_pharmacy` (
 	medication_id int UNIQUE NOT NULL,
 	pharmacy_id int UNIQUE NOT NULL,
 	PRIMARY KEY (medication_id, pharmacy_id,medication_pharmacy_id),
-	CONSTRAINT FOREIGN KEY(`medication_id`) REFERENCES `medication` (`medication_id`)  ON DELETE SET NULL ON UPDATE CASCADE,
-	CONSTRAINT FOREIGN KEY(`pharmacy_id`) REFERENCES `pharmacy` (`pharmacy_id`)  ON DELETE SET NULL ON UPDATE CASCADE
+	CONSTRAINT FOREIGN KEY(`medication_id`) REFERENCES `medication` (`medication_id`) ON DELETE CASCADE,
+	CONSTRAINT FOREIGN KEY(`pharmacy_id`) REFERENCES `pharmacy` (`pharmacy_id`)  ON DELETE CASCADE
 	) ENGINE=InnoDB;
 
 LOCK TABLES `medication_pharmacy` WRITE;
 /*!40000 ALTER TABLE `medication_pharmacy` DISABLE KEYS */;
-INSERT INTO `medication_pharmacy`(medication_id,pharmacy_id) VALUES ('1','1');
+INSERT INTO `medication_pharmacy`(medication_id,pharmacy_id) VALUES ('1','1'),('2','2');
 /*!40000 ALTER TABLE `medication_pharmacy` ENABLE KEYS */;
 UNLOCK TABLES;	
 	
